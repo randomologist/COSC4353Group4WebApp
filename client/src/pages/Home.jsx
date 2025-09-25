eddb20c5d689f12405e759be14d6fcb348b04c9c
spacecheng <spacecheng04@gmail.com>
2025-09-25 15:43:18 -0500

    Fixing a casing error

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import './Home.css'
+
+function Home(){
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <div>This is the Home Page</div>
+        </div>
+    )
+}
+
+export default Home
