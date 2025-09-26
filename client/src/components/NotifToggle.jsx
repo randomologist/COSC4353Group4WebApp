b87495aa7c003831dc126528a7d347dea19228fc
randomologist <randomologist2000@gmail.com>
2025-09-26 14:06:46 -0500

    WIP: notifications debugging

---
+++ client/src/components/NotifToggle.jsx
@@ -0,0 +1,34 @@
+import {useState,useEffect, useRef} from "react";
+import NotifDropdown from "./NotifDropdown";
+import './NotifToggle.css';
+function NotifToggle() {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleRef = useRef(null);
+
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
+
+    useEffect(() => {
+        const unfocus = (event) => {
+            if (toggleRef.current && !toggleRef.current.contains(event.target)) {
+                setIsOpen(false);
+            }
+        };
+        document.addEventListener("mousedown", unfocus);
+        return () => {
+            document.removeEventListener("mousedown", unfocus);
+        };
+    }, []);
+
+    return (
+        <div ref={toggleRef} style ={{ position: "relative", display: "inline-block"}}>
+            <button className="notif-button" onClick={toggleDropdown}>
+                {isOpen ? ">" : "v"} //replace with icons later
+            </button>
+            {isOpen && <NotifDropdown />}
+        </div>
+    );
+}
+
+export default NotifToggle;
