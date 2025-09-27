ff06f06a86f47b3ff911dc8ec46e1865c9bd993c
spacecheng <spacecheng04@gmail.com>
2025-09-26 23:42:02 -0500

    Made Sign up and Login seperated

--- client/src/pages/AuthPage.jsx
+++ client/src/pages/AuthPage.jsx
@@ -1,8 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './AuthPage.css'
 
 function AuthPage(){
-    const [mode, setMode] = useState('login');
+    const [mode, setMode] = useState(() => {
+        const hash = window.location.hash.replace('#', '');
+        return hash === "sign-up" ? "sign-up" : "login";
+
+    });
+    useEffect(() => {
+        const handleHashChange = () => {
+            const hash = window.location.hash.replace('#', '');
+            setMode(hash === "sign-up" ? "sign-up" : "login");
+        }
+    window.addEventListener("hashchange", handleHashChange);
+    return () => window.removeEventListener('hashchange', handleHashChange);
+    }, []);
     const [formData, setFormData] = useState({
         email: '',
         password: '',
@@ -17,23 +29,30 @@ function AuthPage(){
     return (
         <div id="formWrapper">
             <div id="formBody">
-                <label>Email</label>
-                <input
-                name="email"
-                type="text"
-                placeholder="example@domain.com"
-                value={formData.email}
-                onChange={handleChange}
-                />
-                <label>Password</label>
-                <input
-                name="password"
-                type="password"
-                placeholder="password"
-                value={formData.password}
-                onChange={handleChange}
-                />
-                <button>Log in</button>
+                <h2>{mode === "sign-up" ? "Sign Up" : "Log In"}</h2>
+                <div class="section">
+                    <span class="authLabel">Email</span>
+                    <input
+                    name="email"
+                    type="text"
+                    placeholder="example@domain.com"
+                    value={formData.email}
+                    onChange={handleChange}
+                    />
+                </div>
+                <div class="section">
+                    <span class="authLabel">Password</span>
+                    <input
+                    name="password"
+                    type="password"
+                    placeholder="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                    />
+                    {mode === "login" && ( <a id="forgot-pass">Forgot Password</a>)}
+                    </div>
+                <button>{mode === "sign-up" ? "Sign Up" : "Log In"}</button>
+                <a href={mode === "sign-up" ? "#login" : "#sign-up"}>{mode === "sign-up" ? "Already have an account? Log In" : "Don't have an account? Sign Up"}</a>
             </div>
         </div>
     )
