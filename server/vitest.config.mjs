46fa6c0ef32d8743b04e115c16f3dc2c924ece2f
Nicholas Pham <nickt.pham@outlook.com>
2025-10-31 23:05:30 -0500

    More coverage testing fixes

--- server/vitest.config.mjs
+++ server/vitest.config.mjs
@@ -4,6 +4,31 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
-    coverage: { provider: 'v8', reporter: ['text', 'html', 'lcov'] },
+    coverage: { 
+      provider: 'v8', 
+      reporter: ['text', 'html', 'lcov'], 
+
+      all: true,
+      include: [
+        '**/*.js',            
+      ],
+
+    
+      exclude: [
+        '**/node_modules/**',
+        '**/tests/**',
+        '**/*.test.*',
+        'vitest.config.*',
+
+        // Exclude client for testing
+        '../client/**',
+
+        // userStore seems obsolete for now
+        'data/userStore.js',
+        'middleware/roleMiddleware.js', // Potentially add back in later
+      ],
+
+      cleanOnRerun: true,
+    },
   },
 });
