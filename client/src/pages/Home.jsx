3141710952bc0c6e0ec3d42ad81c52a9553143c1
randomologist <randomologist2000@gmail.com>
2025-11-25 18:12:38 -0600

    Home stylistic changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import './Home.css'
-
+import { useAuth } from '../auth/AuthProvider'
 function Home(){
+    const { user } = useAuth();
     return (
         <div>
             <h1>Volunteer Site</h1>
-            <div className="colored-text">A site to register for/set up a volunteer event</div>
+            {!user && (<div className="colored-text">A site to register for/set up a volunteer event<br/>Register for an account to continue</div>)}
+            {user && (<div className="colored-text">Welcome, Thank you for your time</div>)}
         </div>
     )
 }
