9999d74086100b8b362aca307297a99fafa9e98c
randomologist <randomologist2000@gmail.com>
2025-11-28 21:34:17 -0600

    added proper imports for router protections

--- server/routes/matching.js
+++ server/routes/matching.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const authMiddleware = require("../middleware/authMiddleware");
+const roleMiddleware = require("../middleware/roleMiddleware");
 const {
   getAllVolunteers,
   getMatchedEvents,
